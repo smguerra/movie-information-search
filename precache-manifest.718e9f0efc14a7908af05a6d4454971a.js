6a311ca0382c39613408e8bdc01a9cd0b06add8d
Milena <milenagg77@hotmail.com>
2023-03-22 11:50:25 +0000

    Updates

---
+++ precache-manifest.718e9f0efc14a7908af05a6d4454971a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b7ab039a429563349e3",
+    "url": "/movie-information-search/static/js/main.7b7ab039.chunk.js"
+  },
+  {
+    "revision": "1e7934d925d57e33ea02",
+    "url": "/movie-information-search/static/css/1.e3ac222c.chunk.css"
+  },
+  {
+    "revision": "1e7934d925d57e33ea02",
+    "url": "/movie-information-search/static/js/1.1e7934d9.chunk.js"
+  },
+  {
+    "revision": "6cb4aed1c579085bbef0",
+    "url": "/movie-information-search/static/js/runtime~main.6cb4aed1.js"
+  },
+  {
+    "revision": "3e66f217b9376ef4f89b13aea2f5f055",
+    "url": "/movie-information-search/index.html"
+  }
+];
